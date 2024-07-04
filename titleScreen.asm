433a35f328b3d5320e4b83710306258ca8ced6a4
FelipeCarneiroMachado <felipecmachado05@gmail.com>
2024-07-04 17:15:36 -0300

    removidas as referencias ao background e a titlescreen, mas nao compila ainda

--- titleScreen.asm
+++ titleScreen.asm
@@ -43,251 +43,7 @@ yellow : var #1
 blue   : var #1
     static blue   , #3072
 
-; Title screen's variables
-demake       : string "- THE DEMAKE -"
-hiscoreLabel : string "HISCORE "
-press        : string "PRESS "
-enter        : string "ENTER "
-to_play      : string "TO PLAY"
-
-title_length : var #1
-    static title_length, #200
-
-title : var #200
-; Linha 6
-  static title + #0   , #3138
-  static title + #1   , #3138
-  static title + #2   , #2626
-  static title + #3   , #2626
-  static title + #4   , #2626
-  static title + #5   , #2626
-  static title + #6   , #3138
-  static title + #7   , #2626
-  static title + #8   , #2626
-  static title + #9   , #2626
-  static title + #10  , #3138
-  static title + #11  , #3138
-  static title + #12  , #3138
-  static title + #13  , #2626
-  static title + #14  , #2626
-  static title + #15  , #3138
-  static title + #16  , #3138
-  static title + #17  , #3138
-  static title + #18  , #2626
-  static title + #19  , #2626
-  static title + #20  , #2626
-  static title + #21  , #3138
-  static title + #22  , #3138
-  static title + #23  , #2626
-  static title + #24  , #2626
-  static title + #25  , #2626
-  static title + #26  , #3138
-  static title + #27  , #2626
-  static title + #28  , #2626
-  static title + #29  , #2626
-  static title + #30  , #2626
-  static title + #31  , #3138
-  static title + #32  , #2626
-  static title + #33  , #2626
-  static title + #34  , #2626
-  static title + #35  , #3138
-  static title + #36  , #3138
-  static title + #37  , #2626
-  static title + #38  , #3138
-  static title + #39  , #3138
-
-  ;Linha 7
-  static title + #40  , #3138
-  static title + #41  , #3138
-  static title + #42  , #2626
-  static title + #43  , #3138
-  static title + #44  , #3138
-  static title + #45  , #3138
-  static title + #46  , #3138
-  static title + #47  , #2626
-  static title + #48  , #3138
-  static title + #49  , #3138
-  static title + #50  , #2626
-  static title + #51  , #3138
-  static title + #52  , #2626
-  static title + #53  , #3138
-  static title + #54  , #3138
-  static title + #55  , #2626
-  static title + #56  , #3138
-  static title + #57  , #2626
-  static title + #58  , #3138
-  static title + #59  , #3138
-  static title + #60  , #3138
-  static title + #61  , #3138
-  static title + #62  , #2626
-  static title + #63  , #3138
-  static title + #64  , #3138
-  static title + #65  , #3138
-  static title + #66  , #3138
-  static title + #67  , #2626
-  static title + #68  , #3138
-  static title + #69  , #3138
-  static title + #70  , #3138
-  static title + #71  , #3138
-  static title + #72  , #2626
-  static title + #73  , #3138
-  static title + #74  , #3138
-  static title + #75  , #2626
-  static title + #76  , #3138
-  static title + #77  , #2626
-  static title + #78  , #3138
-  static title + #79  , #3138
-
-  ;Linha 8
-  static title + #80  , #3138
-  static title + #81  , #3138
-  static title + #82  , #2626
-  static title + #83  , #2626
-  static title + #84  , #2626
-  static title + #85  , #3138
-  static title + #86  , #3138
-  static title + #87  , #2626
-  static title + #88  , #2626
-  static title + #89  , #2626
-  static title + #90  , #2626
-  static title + #91  , #3138
-  static title + #92  , #2626
-  static title + #93  , #3138
-  static title + #94  , #3138
-  static title + #95  , #2626
-  static title + #96  , #3138
-  static title + #97  , #2626
-  static title + #98  , #3138
-  static title + #99  , #2626
-  static title + #100 , #2626
-  static title + #101 , #3138
-  static title + #102 , #2626
-  static title + #103 , #3138
-  static title + #104 , #2626
-  static title + #105 , #2626
-  static title + #106 , #3138
-  static title + #107 , #2626
-  static title + #108 , #2626
-  static title + #109 , #2626
-  static title + #110 , #3138
-  static title + #111 , #3138
-  static title + #112 , #2626
-  static title + #113 , #2626
-  static title + #114 , #2626
-  static title + #115 , #2626
-  static title + #116 , #3138
-  static title + #117 , #2626
-  static title + #118 , #3138
-  static title + #119 , #3138
-
-  ;Linha 9
-  static title + #120 , #3138
-  static title + #121 , #3138
-  static title + #122 , #2626
-  static title + #123 , #3138
-  static title + #124 , #3138
-  static title + #125 , #3138
-  static title + #126 , #3138
-  static title + #127 , #2626
-  static title + #128 , #3138
-  static title + #129 , #2626
-  static title + #130 , #3138
-  static title + #131 , #3138
-  static title + #132 , #2626
-  static title + #133 , #3138
-  static title + #134 , #3138
-  static title + #135 , #2626
-  static title + #136 , #3138
-  static title + #137 , #2626
-  static title + #138 , #3138
-  static title + #139 , #3138
-  static title + #140 , #2626
-  static title + #141 , #3138
-  static title + #142 , #2626
-  static title + #143 , #3138
-  static title + #144 , #3138
-  static title + #145 , #2626
-  static title + #146 , #3138
-  static title + #147 , #2626
-  static title + #148 , #3138
-  static title + #149 , #3138
-  static title + #150 , #3138
-  static title + #151 , #3138
-  static title + #152 , #2626
-  static title + #153 , #3138
-  static title + #154 , #2626
-  static title + #155 , #3138
-  static title + #156 , #3138
-  static title + #157 , #3138
-  static title + #158 , #3138
-  static title + #159 , #3138
-
-  ;Linha 10
-  static title + #160 , #3138
-  static title + #161 , #3138
-  static title + #162 , #2626
-  static title + #163 , #3138
-  static title + #164 , #3138
-  static title + #165 , #3138
-  static title + #166 , #3138
-  static title + #167 , #2626
-  static title + #168 , #3138
-  static title + #169 , #3138
-  static title + #170 , #2626
-  static title + #171 , #3138
-  static title + #172 , #3138
-  static title + #173 , #2626
-  static title + #174 , #2626
-  static title + #175 , #3138
-  static title + #176 , #3138
-  static title + #177 , #2626
-  static title + #178 , #2626
-  static title + #179 , #2626
-  static title + #180 , #2626
-  static title + #181 , #3138
-  static title + #182 , #2626
-  static title + #183 , #2626
-  static title + #184 , #2626
-  static title + #185 , #2626
-  static title + #186 , #3138
-  static title + #187 , #2626
-  static title + #188 , #2626
-  static title + #189 , #2626
-  static title + #190 , #2626
-  static title + #191 , #3138
-  static title + #192 , #2626
-  static title + #193 , #3138
-  static title + #194 , #3138
-  static title + #195 , #2626
-  static title + #196 , #3138
-  static title + #197 , #2626
-  static title + #198 , #3138
-  static title + #199 , #3138
-
-; How to play
-how_to_play0  : string "HOW TO PLAY"
-how_to_play1  : string "W - "
-how_to_play2  : string "MOVE UP"
-how_to_play3  : string "A - "
-how_to_play4  : string "MOVE LEFT"
-how_to_play5  : string "S - "
-how_to_play6  : string "MOVE DOWN"
-how_to_play7  : string "D - "
-how_to_play8  : string "MOVE RIGHT"
-
-; How to Score
-how_to_score0 : string "HOW TO SCORE"
-how_to_score1 : string "1 PT "
-how_to_score2 : string "FOR EVERY LEAP FORWARD"
-how_to_score3 : string "5 PTS "
-how_to_score4 : string "FOR EVERY FROG THAT CROSSED"
-how_to_score5 : string "100 PTS "
-how_to_score6 : string "FOR CROSSING ALL FROGS"
-how_to_score7 : string "1 PT "
-how_to_score8 : string "FOR EVERY REMAINING SECOND"
-
 ; Matrices to store background and foreground info, respectively
-background : var #1200
 foreground : var #1200
 
 ; Data segment to store in game status information
@@ -524,114 +280,6 @@ restoreRegisters:
     loadn r0, #0
     rts
 
-initTitleScreen:
-    ; Prints the background of the title screen.
-    ; a1 = Pointer to background map where to store printed characters
-    ; Returns: Nothing
-
-    call saveRegisters
-
-    ; Wipe Map
-    store a1, r0
-    store a2, r0
-    store a3, r1
-    load r1, WIDTH
-    load r2, HEIGHT
-    mul r1, r1, r2
-    store a4, r1
-    store a5, r0
-    call printChar
-
-    ; Print Highscore indicator
-    loadn r1, #hiscoreLabel
-    store a1, r1
-    loadn r1, #13
-    store a2, r1
-    store a4, r0
-    call printString
-
-    load r2, a0
-    add r1, r1, r2
-    store a2, r1
-    loadn r1, #'0'
-    load r2, red
-    add r1, r1, r2
-    store a1, r1
-    loadn r1, #5
-    store a4, r1
-    call printChar
-
-    ; Print "Frogger!"
-    load r1, FILL
-    load r2, blue
-    add r1, r1, r2
-    store a1, r1 ; Fill character with added color
-    loadn r2, #80
-    store a2, r2
-    loadn r3, #2
-    mul r3, r3, r2
-    store a4, r3 ; Characters to print
-    call printChar
-
-    loadn r4, #title
-    store a1, r4
-    add r2, r2, r3
-    store a2, r2
-    load r3, title_length
-    store a4, r3
-    call printVector
-
-    store a1, r1
-    add r1, r2, r3
-    store a2, r1
-    load r1, WIDTH
-    store a4, r1
-    call printChar
-
-    ; Print "- The Demake -"
-    loadn r1, #13
-    store a1, r1
-    store a2,r1
-    call screenOffset
-    loadn r1, #demake
-    store a1, r1
-    load r1, a0
-    store a2, r1
-    store a4, r0
-    call printString
-
-    ; Print "Press enter to play"
-    loadn r1, #26
-    store a1, r1
-    loadn r1, #10
-    store a2, r1
-    call screenOffset
-    loadn r1, #press
-    store a1, r1
-    load r1, a0
-    store a2, r1
-    call printString
-
-    loadn r2, #enter
-    store a1, r2
-    load r2, a0
-    add r1, r1, r2
-    store a2, r1
-    load r2, yellow
-    store a4, r2
-    call printString
-
-    loadn r2, #to_play
-    store a1, r2
-    load r2, a0
-    add r1, r1, r2
-    store a2, r1
-    store a4, r0
-    call printString
-
-    call restoreRegisters
-    rts
-
 
 takeInput:
     ; A function that delays the game's execution for a given period to take an input from the player. If the input is one that was expected, the delay is canceled.
@@ -863,132 +511,6 @@ printString:
         rts
 
 
-printInstructions:
-    ; Prints a left aligned text box containing instructions on how to play or score and returns whether the user has interrupted printing or not.
-    ; Arguments:
-    ; a1 = address of the vector of strings to be printed
-    ; a2 = number of lines to be printed
-    ; a3 = map to save printed characters
-    ; a4 = leftmost column from where to start printing instructions
-    ; Returns:
-    ; a0 = 0 if printing was interrupted, otherwise 1.
-
-    call saveRegisters
-    store a0, r0
-
-    ; Print a centered header
-    loadn r5, #16
-    store a1, r5
-    loadn r5, #14
-    store a2, r5
-    call screenOffset
-    load r5, a0
-    store a1, r1
-    store a2, r5
-    store a4, r0
-    load r3, ENTER
-    store a5, r3
-    call printString
-    load r3, a0
-    cmp r3, r0
-    jeq piEnd
-    add r1, r1, r3
-    inc r1
-    add r5, r5, r3
-
-    ; Pad right
-    store a1, r0
-    store a2, r5
-    loadn r3, #1
-    store a4, r3
-    call printChar
-    load r3, a0
-    cmp r3, r0
-    jeq piEnd
-    dec r2
-
-    ; Set initial values
-    loadn r5, #18
-    store a1, r5
-    store a2, r0
-    call screenOffset
-    load r5, a0
-    load r6, WIDTH
-
-    piBody:
-        cmp r2, r0
-        jeq piCompleted
-
-        ; Pad left
-        store a1, r0
-        store a2, r5
-        store a4, r4
-        call printChar
-        load r7, a0
-        cmp r7, r0
-        jeq piEnd
-
-        ; Offset screen pointer
-        add r5, r5, r7
-
-        ; Print with emphasis (red)
-        store a1, r1
-        store a2, r5
-        load r7, red
-        store a4, r7
-        call printString
-        load r7, a0
-        cmp r7, r0
-        jeq piEnd
-
-        ; Offset string and screen pointer
-        add r1, r1, r7
-        inc r1
-        add r5, r5, r7
-
-        ; Print description (yellow)
-        store a1, r1
-        store a2, r5
-        load r7, yellow
-        store a4, r7
-        call printString
-        load r7, a0
-        cmp r7, r0
-        jeq piEnd
-
-        ; Offset string and screen pointer
-        add r1, r1, r7
-        inc r1
-        add r5, r5, r7
-
-        ; Pad right
-        store a1, r0
-        store a2, r5
-        mod R7, R5, R6
-        sub R7, R6, R7
-        dec r7
-        store a4, R7
-        call printChar
-        load r7, a0
-        cmp r7, r0
-        jeq piEnd
-
-        ; Offset screen pointer
-        add r5, r5, r7
-        inc r5
-        add r5, r5, r6
-
-        dec r2
-        jmp piBody
-
-    piCompleted:
-        store a0, r3
-
-    piEnd:
-        call restoreRegisters
-        rts
-
-; NOTE: Drawing functions
 
 printEnemy:
     ; a1 = charmap
@@ -1056,19 +578,20 @@ eraseEnemy:
     loadn r5, #0
     push r3
     eraseEnemyLoop1:
-      add r6, r1, r5
-      loadi r6, r6
-      cmp r6, r0
+      add r6, r1, r5 ;r6 tem add do charmap
+      loadi r6, r6 ;r6 tem o char
+      cmp r6, r0 ; fim da linha
       jeq eraseEnemypt2
       store a1, r2
       store a2, r3
-      call screenOffset
+      call screenOffset ;off da coordenada em r4
       load r4, a0
       loadn r7, #foreground
-      add r7, r7, r4
-      loadn r6, #background
+      add r7, r7, r4 
+      ;loadn r6, #background
       add r6, r6, r4
-      loadi r6, r6
+      loadi r6, r6 ;r6 tem o char do background
+      loadn r6, #0 ;Background nulo
       outchar r6, r4
       storei r7, r6
       inc r3
@@ -1095,9 +618,10 @@ eraseEnemy:
         loadn r7, #foreground
         add r7, r7, r4
         storei r7, r0
-        loadn r6, #background
+        ;loadn r6, #background
         add r6, r6, r4
         loadi r6, r6
+        loadn r6, #0
         outchar r6, r4
         inc r3
         loadn r7, #40
@@ -1231,106 +755,6 @@ drawHUD:
     call restoreRegisters
     rts
 
-drawBackground:
-    ; Draw the game's background
-    ; Arguments:
-    ; a1 = pointer to the background vector
-
-    call saveRegisters
-    store a3, r1
-    store a5, r0
-    load r1, FILL     ; Character to print
-    loadn r2, #2      ; Vertical length of the ponds, river margin and the sidewalk
-    store a1, r2
-    store a2, r0
-    call screenOffset
-    load r3, a0       ; Cursor position on the screen
-    breakp
-
-    ; Draw ponds
-    load r4, grass
-    add r4, r1, r4
-    store a1, r4
-    store a2, r3
-    store a4, r2
-    call printChar
-    load r4, a0
-    add r3, r3, r4
-
-    loadn r4, #4
-    store a4, r4
-    loadn r4, #19
-    pondLoop:
-        mod r5, r4, r2
-        jz selectGrass
-            load r5, blue
-            jmp colorSelected
-        selectGrass:
-            load r5, grass
-        colorSelected:
-            add r5, r1, r5
-            store a1, r5
-            store a2, r3
-            call printChar
-            load r5, a0
-            add r3, r3, r5
-
-            dec r4
-            jz pondLoop
-
-    load r4, grass
-    add r4, r1, r4
-    store a1, r4
-    store a2, r3
-    store a4, r2
-    call printChar
-    load r4, a0
-    add r3, r3, r4
-
-    load r4, WIDTH
-    loadn r5, #10 ; Vertical length of the river and the road
-    ; Draw river
-    load r6, blue
-    add r6, r1, r6
-    store a1, r6
-    store a2, r3
-    mul r6, r4, r5
-    store a4, r6
-    call printChar
-    load r6, a0
-    add r3, r3, r6
-
-    ; Draw grass
-    load r6, grass
-    add r6, r1, r6
-    store a1, r6
-    store a2, r5
-    mul r6, r2, r4
-    store a4, r6
-    call printChar
-    load r6, a0
-    add r3, r3, r6
-
-    ; Draw asphalt
-    store a1, r0
-    store a2, r3
-    mul r5, r4, r5
-    store a4, r5
-    call printChar
-    load r5, a0
-    add r3, r3, r5
-
-    ; Draw sidewalk
-    load r5, gray
-    add r5, r1, r5
-    store a1, r5
-    store a2, r3
-    mul r2, r2, r4
-    store a4, r2
-    call printChar
-
-    call restoreRegisters
-    rts
 
 drawGameOver:
     ; Draws a "Game Over" disclaimer that lingers for some time (unless interrupted) before returning the player to the Title Screen
@@ -1404,58 +828,6 @@ drawGameOver:
 
 ; NOTE: Game functions
 
-titleScreen:
-    ; Prints the title screen, displaying game instructions.
-    ; Arguments:
-    ; a1 = current Highscore
-    ; a2 = pointer to background vector
-    ; Returns: Nothing
-
-    call saveRegisters
-    store a1, r2
-    call initTitleScreen
-
-    store a1, r1
-    store a3, r2
-    loadn r1, #25         ; Column offset
-    store a2, r1
-    load r1, red
-    store a4, r1
-    call printInt         ; Print Highscore
-
-    mov r1, r0            ; variable to toggle between instructions to be displayed
-    load r2, ENTER        ; variable to compare if ENTER was pressed
-    loadn r3, #30000      ; Set delay interval to 3 seconds
-    loadn r4, #5          ; Set number of lines of instructions to be printed
-
-    titleScreenLoop:
-        cmp r1, r0        ; Either print instructions for scoring or playing
-        jeq selectPlay    ; Selection logic
-        loadn r5, #how_to_score0
-        loadn r6, #2      ; Column offset
-        jmp instructionSelected
-        selectPlay:
-            loadn r5, #how_to_play0
-            loadn r6, #13 ; Column offset
-        instructionSelected:
-            store a1, r5
-            store a2, r4
-            store a4, r6
-            call printInstructions
-            load r5, a0
-            cmp r5, r0
-            jeq titleScreenEnd
-            store a1, r3
-            store a2, r2
-            call takeInput
-            load r5, a0
-            cmp r5, r2
-            jeq titleScreenEnd
-            not r1, r1
-            jmp titleScreenLoop
-    titleScreenEnd:
-        call restoreRegisters
-        rts
 
 gameScreen:
     ; Function to execute the game itself.
@@ -1470,7 +842,6 @@ gameScreen:
     call initStats
     store a1, r2
     call drawHUD
-    call drawBackground
     ; TODO: Create and call function drawLanes using the drawCharmap as an auxiliary function
 
     load r4, lives
@@ -1719,22 +1090,14 @@ fn_eraseFrog:
 
     call saveRegisters
     load r1, frog_pos
-    loadn r2, #background
-    loadi r3, r2
-    outchar r3, r1 ;Top left
+    outchar r0, r1 ;Top left
     inc r1
-    inc r2
-    loadi r3, r2
-    outchar r3, r1 ;Top Right
+    outchar r0, r1 ;Top Right
     loadn r4, #39
     add r1, r1, r4
-    add r2, r2, r4
-    loadi r3, r1
-    outchar r3, r1 ;Bottom left
+    outchar r0, r1 ;Bottom left
     inc r1
-    inc r2
-    loadi r3, r2
-    outchar r3, r1 ;Bottom right
+    outchar r0, r1 ;Bottom right
     call restoreRegisters
     rts
 
@@ -2026,43 +1389,62 @@ fn_subLives:
     ;Returns 0 if there are still lives
     ;Returns 1 if game over
     call saveRegisters
-    loadn r1, #lanes
-    loadi r1, r1
-    load r2, HEIGHT
-    loadn r3, #4
-    sub r2, r2, r3
-    loadn r3, #2
+    load r1, lives
+    dec r1
+    store lives, r1
+    cmp r1, r0
+    jeq slDead 
+    store a0, r0
+    call restoreRegisters
+    rts
+    slDead:
+        loadn r1, #1
+        store a0, r1
+        call restoreRegisters
+        rts
 
-    ; Setup Yellow
-    sub r3, r3, r4
-    storei r2, r3
-    inc r2
+    
 
-    loadn r5, #16
-    storei r2, r5
-    inc r2
 
-    loadn r5, #3
-    storei r2, r5
-    inc r2
+    ;Codigo fora de lugar
 
-    loadn r5, #50
-    storei r2, r5
-    inc r2
+    ; loadn r1, #lanes
+    ; loadi r1, r1
+    ; load r2, HEIGHT
+    ; loadn r3, #4
+    ; sub r2, r2, r3
+    ; loadn r3, #2
 
-    storei r2, r0
-    inc r2
+    ; ; Setup Yellow
+    ; sub r3, r3, r4
+    ; storei r2, r3
+    ; inc r2
+
+    ; loadn r5, #16
+    ; storei r2, r5
+    ; inc r2
+
+    ; loadn r5, #3
+    ; storei r2, r5
+    ; inc r2
+
+    ; loadn r5, #50
+    ; storei r2, r5
+    ; inc r2
+
+    ; storei r2, r0
+    ; inc r2
 
 main:
     loadn r0, #0 ; Set r0 to 0, this register should hold this value always
     load r1, hiscore
-    loadn r2, #background
-    loadn r3, #foreground
+    ;loadn r2, #background
+    ;loadn r3, #foreground
     mainLoop:
 
-        store a1, r1
-        store a2, r2
-        call titleScreen
+        ;store a1, r1
+        ;store a2, r2
+        ;call titleScreen
 
         store a1, r1
         store a2, r2
